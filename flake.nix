18729d81bad3fd0a8b70f5f7057c82cccc28be2d
shivangjhalani <shivang2004jhalani@gmail.com>
2024-02-01 00:15:24 +0530

    first commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Home Manager configuration of sjay";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+	nixos = lib.nixosSystem {
+	  modules = [ ./configuration.nix ];
+	};
+      };
+      homeConfigurations = {
+	sjay = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+  };
+}
