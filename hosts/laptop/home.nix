bf20e4c452bc562756023cc991eeafc7117cb371
shivangjhalani <shivang2004jhalani@gmail.com>
2024-02-01 19:45:04 +0530

    hm

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -22,8 +22,10 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    # Core
     git
     firefox
+    
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
